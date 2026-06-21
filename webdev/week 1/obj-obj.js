47d2f544ebfee6d05a5b9aeab5b907972d603746
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-06-21 16:12:36 +0530

    obj-in-obj

---
+++ webdev/week 1/obj-obj.js
@@ -0,0 +1,12 @@
+const user1 = {
+  name: "priyanshu",
+  age: 20,
+  email: "bharti",
+  address: {
+    street: "123 Main St",
+    city: "New York",
+    country: "USA"
+  }
+};
+
+console.log(user1.name);
