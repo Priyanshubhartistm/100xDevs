2ca4ea3fae82bdf8a4046de8b772c6c6c3048737
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-07-06 03:08:15 +0530

    assessement

---
+++ webdev/week 1/ass.2.js
@@ -0,0 +1,20 @@
+/*
+- Assignment #2
+    
+    Write a function `sum` that finds the sum of two numbers. 
+    Side quest - Try passing in a string instead of a number and see what happens?
+*/
+
+// function sum(a,b) {
+//     return a + b;
+// }
+
+// console.log(sum(5,10));
+
+
+function add(a,b) {
+    return a + b;
+
+}
+
+console.log(add(2,3));
