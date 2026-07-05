00a753ec2b6ef0cacd37eaf8f358f990481499f5
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-07-06 03:07:44 +0530

    assessement

---
+++ webdev/week 1/ass-1.js
@@ -0,0 +1,12 @@
+/*
+- Assignment #2
+Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). 
+Try logging it using console.log
+*/
+let color = "black";
+let height = 152;
+let pizza = true;
+
+console.log(color);
+console.log(height);
+console.log(pizza);
